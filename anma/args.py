3df78224268f8a8709198c64a874cbc44ce56c45
Gabriel Duque <gabriel.duque@lse.epita.fr>
2019-03-24 18:14:26 +0100

    anma: add argument parser

---
+++ anma/args.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("-o", "--output", help="set output file",
+                        type=str, default="out.mid")
+    parser.add_argument("-c", "--conf", help="set keymap configuration file",
+                        type=str, default="misc/anma.conf")
+    parser.add_argument("-b", "--background", help="set background image",
+                        type=str, default="misc/cat.png")
+    parser.add_argument("-M", "--midi_output", help="set MIDI output id",
+                        type=int, default=0)
+    args = parser.parse_args()
+    return args
+
+def dump_config(arg):
+    print(
+            f'Verbose: {"on" if arg.verbose else "off"}\n'
+            f'Output file: {arg.output}\n'
+            f'Keymap configuration: {arg.conf}\n'
+            f'Background image: {arg.background}\n'
+            f'Midi output identifier: {arg.midi_output}'
+    )
